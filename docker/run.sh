98f62f63d9e0aaa6e35ed2a8657554fb4b38ad7c
Dino <leapersedge@gmail.com>
2025-12-11 17:46:28 +0100

    dockerfile fixed but wrong gtsam-eigen version

--- docker/run.sh
+++ docker/run.sh
@@ -3,13 +3,9 @@ trap : SIGTERM SIGINT
 
 function abspath() {
     # generate absolute path from relative path
-    # $1     : relative filename
-    # return : absolute path
     if [ -d "$1" ]; then
-        # dir
         (cd "$1"; pwd)
     elif [ -f "$1" ]; then
-        # file
         if [[ $1 = /* ]]; then
             echo "$1"
         elif [[ $1 == */* ]]; then
@@ -25,37 +21,30 @@ if [ "$#" -ne 1 ]; then
   exit 1
 fi
 
-roscore &
-ROSCORE_PID=$!
-sleep 1
-
-rviz -d ../config/vins_rviz_config.rviz &
+# Launch RViz in background
+rviz2 -d ../config/vins_rviz_config.rviz &
 RVIZ_PID=$!
 
 VINS_MONO_DIR=$(abspath "..")
 
+# Run VINS-Mono inside Jazzy Docker
 docker run \
   -it \
   --rm \
   --net=host \
-  -v ${VINS_MONO_DIR}:/root/catkin_ws/src/VINS-Mono/ \
-  ros:vins-mono \
-  /bin/bash -c \
-  "cd /root/catkin_ws/; \
-  catkin config \
-        --env-cache \
-        --extend /opt/ros/$ROS_DISTRO \
-       --cmake-args \
-         -DCMAKE_BUILD_TYPE=Release; \
-     catkin build; \
-     source devel/setup.bash; \
-     roslaunch vins_estimator ${1}"
+  -v ${VINS_MONO_DIR}:/root/ros2_ws/src/VINS-Mono/ \
+    jazzy-vins-mono \
+    /bin/bash -c "\
+    cd /root/ros2_ws/; \
+    source /opt/ros/jazzy/setup.bash; \
+    colcon build --symlink-install; \
+    source install/setup.bash; \
+    ros2 launch vins_estimator ${1} \
+  "
 
-wait $ROSCORE_PID
 wait $RVIZ_PID
 
-if [[ $? -gt 128 ]]
-then
-    kill $ROSCORE_PID
+if [[ $? -gt 128 ]]; then
     kill $RVIZ_PID
 fi
+
